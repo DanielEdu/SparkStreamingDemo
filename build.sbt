f5d397da7ade6f099a3c02ffdf3fb79fb32eac3f
danieledu <dportuga@everis.com>
2020-02-22 19:20:38 -0500

    prueba de Spark + Kafka -> ok!

--- build.sbt
+++ build.sbt
@@ -7,13 +7,29 @@ scalaVersion := "2.11.12"
 
 val sparkVersion = "2.4.0"
 
-libraryDependencies ++= Seq(
-  "org.apache.spark" %% "spark-core" % sparkVersion % "provided",
-  "org.apache.spark" %% "spark-sql" % sparkVersion % "provided"
-)
+libraryDependencies ++= {
+  val sparkV = "2.3.4"
+  val cassandraV = "2.0.5"
+  Seq(
+    "org.apache.spark" %% "spark-core" % sparkV,
+    "org.apache.spark" %% "spark-streaming" % sparkV,
+    "org.apache.spark" %% "spark-streaming-kafka-0-10" % sparkV,
+    "org.apache.spark" %% "spark-sql-kafka-0-10" % sparkV,
+    "org.apache.spark" %% "spark-sql" % sparkV,
+    "org.apache.spark" %% "spark-hive" % sparkV,
+    "com.datastax.spark" %% "spark-cassandra-connector" % cassandraV,
+    "org.scalatest" %% "scalatest" % "3.0.3" % "test",
+    "com.github.nscala-time" %% "nscala-time" % "2.12.0",
+    "com.typesafe" % "config" % "1.3.1",
+    "com.holdenkarau" %% "spark-testing-base" % "2.2.0_0.7.2",
+    "com.datastax.cassandra" % "cassandra-driver-core" % "3.3.0"
+  )
+}
 
-mainClass in assembly := some("com.demo.streaming.BatchDemo")
-assemblyJarName := "StreamingDemo.jar"
+
+
+mainClass in assembly := some("com.demo.streaming.KafkaToSpark")
+assemblyJarName := "StructuredStreamingDemo.jar"
 
 assemblyMergeStrategy in assembly := {
   case PathList("META-INF", xs @ _*) => MergeStrategy.discard
--- src/main/scala/com/demo/streaming/BatchDemo.scala
+++ src/main/scala/com/demo/streaming/BatchDemo.scala
@@ -39,12 +39,55 @@ object BatchDemo {
 
     // we need to narrow the `Interger` type because
     // the JSON representation is interpreted as `BigInteger`
-    val preparedLogs = rawLogs.withColumn("http_reply", col("http_reply").cast(IntegerType))
+    val preparedLogs = rawLogs
+      .withColumn("http_reply", col("http_reply")
+        .cast(IntegerType)
+      )
+      .filter("http_reply is not null")
 
     import spark.implicits._
     val weblogs = preparedLogs.as[WebLog]
 
-    weblogs.count()
+    weblogs.sort($"http_reply".asc).show(5,true)
+
+    val topDailyURLs =  weblogs
+      .withColumn("DayOfMonth",dayofmonth($"timestamp"))
+      .select($"DayOfMonth",$"request")
+      .groupBy($"DayOfMonth",$"request")
+      .agg(count($"request").alias("Count"))
+      .orderBy($"Count".asc)
+
+    //TOP 10
+    topDailyURLs.show(10,false)
+
+
+    //  Extraer GET y dejar .html y .htm
+    val urlExtractor = """^GET (.+) HTTP/\d.\d""".r
+    val allowedExtensions = Set(".html",".htm", "")
+
+    val contentPageLogs = weblogs.filter {log =>
+      log.request match {
+        case urlExtractor(url) =>
+          val ext = url.takeRight(5).dropWhile(c => c != '.')
+          allowedExtensions.contains(ext)
+        case _ => false
+      }
+    }
+
+    val topContentPages =  contentPageLogs
+      .withColumn("dayOfMonth", dayofmonth($"timestamp"))
+      .select($"request", $"dayOfMonth")
+      .groupBy($"dayOfMonth", $"request")
+      .agg(count($"request").alias("count"))
+      .orderBy(desc("count"))
+
+    println("-----------------   ahora si  ------------------")
+    topContentPages.show(10,false)
+
+
+    println("iniciar server...")
+
+
 
 
   }
